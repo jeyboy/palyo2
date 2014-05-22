d7b73efb716108ec53e90e79a5536b3b58e00add
Jenua Boiko <jeyboy1985@gmail.com>
2014-05-23 03:18:38 +0400

    temp

--- web/socials/vk_api.cpp
+++ web/socials/vk_api.cpp
@@ -184,29 +184,4 @@ void VkApi::init(QString pToken, QString pUserId, QString pExpired) {
     token = pToken;
     user_id = pUserId;
     expires_in = pExpired;
-
-    genres = QHash<int, QString>();
-
-    genres.insert(1, "Rock");
-    genres.insert(2, "Pop");
-    genres.insert(3, "Rap & Hip-Hop");
-    genres.insert(4, "Easy Listening");
-    genres.insert(5, "Dance & House");
-    genres.insert(6, "Instrumental");
-    genres.insert(7, "Metal");
-    genres.insert(8, "Dubstep");
-    genres.insert(9, "Jazz & Blues");
-    genres.insert(10, "Drum & Bass");
-    genres.insert(11, "Trance");
-    genres.insert(12, "Chanson");
-    genres.insert(13, "Ethnic");
-    genres.insert(14, "Acoustic & Vocal");
-    genres.insert(15, "Reggae");
-    genres.insert(16, "Classical");
-    genres.insert(17, "Indie Pop");
-    genres.insert(18, "Other");
-    genres.insert(19, "Speech");
-
-    genres.insert(21, "Alternative");
-    genres.insert(22, "Electropop & Disco");
 }
