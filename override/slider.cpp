c6366c1fcf518108fba1fbadb705e24dc53da7d1
Jenua Boiko <jeyboy1985@gmail.com>
2014-07-15 23:46:09 +0400

    slider refCTOR

--- override/slider.cpp
+++ override/slider.cpp
@@ -25,29 +25,28 @@ void Slider::paintEvent(QPaintEvent * event) {
     p.save();
 
     p.setPen(QColor::fromRgb(0, 0, 0));
-    QRect rect = this -> geometry();
+    QRect rect = this -> rect();
+
     QString strNum;
 
-    double limit, temp = 0, step = ((double)maximum()) / tickInterval();
+    double temp = 0, step = ((double)maximum()) / tickInterval();
     int multiplyer = 0;
 
     if (orientation() == Qt::Horizontal) {
-        rect.moveLeft(rect.left() + margin);
-        rect.setWidth(rect.width() - margin);
+        rect.moveLeft(rect.left() + margin + 1); // +1 border
+        rect.setWidth(rect.width() - margin * 2 - 2); // -2 border
 
         while(temp < 16) {
-            multiplyer++;
-            temp = ((float)(rect.width())) / (step / multiplyer);
+            temp = ((float)(rect.width())) / (step / ++multiplyer);
         }
 
         step = temp;
-        limit = (rect.width() / step) == 0 ? rect.width() - step : rect.width();
-        int bottom = rect.bottom() - 7, h = (rect.height() / 3) - 3;
+        int bottom = rect.bottom() - 6, h = (rect.height() / 3) - 3;
         double val = multiplyer;
 
-        for(double pos = step; pos < limit; pos += step, val += multiplyer) {
+        for(double pos = step + rect.left(); pos <= rect.right() + 0.5; pos += step, val += multiplyer) {
             strNum = QString::number(val);
-            p.drawLine(pos, bottom - h, pos, bottom);
+            p.drawLine(QLineF(pos, bottom - h, pos, bottom));
             if (position_slider)
                 p.drawText(pos - 7 * strNum.length() , bottom, strNum);
         }
@@ -55,28 +54,29 @@ void Slider::paintEvent(QPaintEvent * event) {
         if (position_slider) {
             float pos = Player::instance() -> getRemoteFileDownloadPosition();
             if (Player::instance() -> getSize() > 0 && pos < 1) {
-                p.drawRect(margin, rect.y(), rect.width() - margin - 1, 3);
-                p.fillRect(margin, rect.y(), (rect.width() - margin - 1) * pos, 3, fillColor);
+                p.drawRect(rect.left(), rect.y(), rect.width() - 1, 3);
+                p.fillRect(rect.left(), rect.y(), (rect.width() - 1) * pos, 3, fillColor);
             }
         }
     } else {
-        rect.moveTop(rect.top() + margin);
-        rect.setHeight(rect.height() - margin);
+        rect.moveTop(rect.top() + margin + 1); // +1 border
+        rect.setHeight(rect.height() - margin * 2 - 2); // -2 border
 
         while(temp < 16) {
-            multiplyer++;
-            temp = ((float)(rect.height())) / (step / multiplyer);
+            temp = ((float)(rect.height())) / (step / ++multiplyer);
         }
 
         step = temp;
-        limit = (rect.height() / step) == 0 ? rect.height() - step : rect.height();
-        int temp, left = rect.left() + 4, w = (rect.width() / 3) - 3;
+        int temp, left = rect.left() + 6, w = (rect.width() / 3) - 3;
         double val = multiplyer;
 
-        for(double pos = step - margin; pos < limit; pos += step, val += multiplyer) {
+        for(double pos = step; ; pos += step, val += multiplyer) {
             strNum = QString::number(val);
-            temp = rect.height() - pos;
-            p.drawLine(left, temp, left + w, temp);
+            temp = rect.bottom() - pos;
+            if (temp < rect.top() - 0.5)
+                break;
+
+            p.drawLine(QLineF(left, temp, left + w, temp));
             if (position_slider)
                 p.drawText(left, temp + 10, strNum);
         }
@@ -84,8 +84,8 @@ void Slider::paintEvent(QPaintEvent * event) {
         if (position_slider) {
             float pos = Player::instance() -> getRemoteFileDownloadPosition();
             if (Player::instance() -> getSize() > 0 && pos < 1) {
-                p.drawRect(rect.x(), margin, 3, rect.height() - margin - 1);
-                p.fillRect(rect.x(), rect.height(), 3, -((rect.height() - margin - 1) * pos), fillColor);
+                p.drawRect(rect.x(), rect.bottom(), 3, -(rect.height() - 1));
+                p.fillRect(rect.x(), rect.bottom(), 3, -((rect.height() - 1) * pos), fillColor);
             }
         }
     }
