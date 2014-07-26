259bf33909d7eb46c04bb1a1a1815e7f813dcff9
Jenua Boiko <jeyboy1985@gmail.com>
2014-07-27 03:29:59 +0400

    temp

--- media/media_player.h
+++ media/media_player.h
@@ -2,6 +2,7 @@
 #define MEDIA_PLAYER_H
 
 #include <QWidget>
+#include <QUrl>
 
 extern "C" {
     #include "media_player_libs.h"
@@ -16,6 +17,69 @@ class MediaPlayer : public QWidget {
 public:
 
     MediaPlayer(QWidget * parent = 0);
+
+protected:
+    bool openObject(QUrl url);
+    void closeObject();
+
+    bool nextFrame();
+
+private:
+
+//    /* options specified by the user */
+//    AVInputFormat *file_iformat;
+//    const char *input_filename;
+////    const char * window_title;
+//    int fs_screen_width;
+//    int fs_screen_height;
+//    int default_width  = 640;
+//    int default_height = 480;
+//    int screen_width  = 0;
+//    int screen_height = 0;
+//    int audio_disable;
+//    int video_disable;
+//    int subtitle_disable;
+//    int wanted_stream[AVMEDIA_TYPE_NB];
+//    int seek_by_bytes = -1;
+//    int display_disable;
+//    int show_status = 1;
+//    int av_sync_type = AV_SYNC_AUDIO_MASTER;
+//    int64_t start_time = AV_NOPTS_VALUE;
+//    int64_t duration = AV_NOPTS_VALUE;
+//    int workaround_bugs = 1;
+//    int fast = 0;
+//    int genpts = 0;
+//    int lowres = 0;
+//    int decoder_reorder_pts = -1;
+//    int autoexit;
+//    int exit_on_keydown;
+//    int exit_on_mousedown;
+//    int loop = 1;
+//    int framedrop = -1;
+//    int infinite_buffer = -1;
+//    enum ShowMode show_mode = SHOW_MODE_NONE;
+//    const char *audio_codec_name;
+//    const char *subtitle_codec_name;
+//    const char *video_codec_name;
+//    double rdftspeed = 0.02;
+//    int64_t cursor_last_shown;
+//    int cursor_hidden = 0;
+//    #if CONFIG_AVFILTER
+//    const char **vfilters_list = NULL;
+//    int nb_vfilters = 0;
+//    char *afilters = NULL;
+//    #endif
+//    int autorotate = 1;
+
+//    int is_full_screen;
+//    int64_t audio_callback_time;
+
+//    AVPacket flush_pkt;
+
+    AVFormatContext * currObj;
+    AVPacket * currFrame;
+    QWidget * screen;
+    bool isRemote;
 };
 
 #endif // MEDIA_PLAYER_H
--- media/media_player_libs.h
+++ media/media_player_libs.h
@@ -1,7 +1,35 @@
 #ifndef MEDIA_PLAYER_LIBS_H
 #define MEDIA_PLAYER_LIBS_H
 
-#include "libavcodec/avcodec.h"
+#include <inttypes.h>
+#include <math.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdint.h>
+
+#include "libavutil/avstring.h"
+#include "libavutil/colorspace.h"
 #include "libavutil/mathematics.h"
+#include "libavutil/pixdesc.h"
+#include "libavutil/imgutils.h"
+#include "libavutil/dict.h"
+#include "libavutil/parseutils.h"
+#include "libavutil/samplefmt.h"
+#include "libavutil/avassert.h"
+#include "libavutil/time.h"
+#include "libavformat/avformat.h"
+#include "libavdevice/avdevice.h"
+#include "libswscale/swscale.h"
+#include "libavutil/opt.h"
+#include "libavcodec/avfft.h"
+#include "libavcodec/avcodec.h"
+#include "libswresample/swresample.h"
+
+#if CONFIG_AVFILTER
+# include "libavfilter/avcodec.h"
+# include "libavfilter/avfilter.h"
+# include "libavfilter/buffersink.h"
+# include "libavfilter/buffersrc.h"
+#endif
 
 #endif // MEDIA_PLAYER_LIBS_H
